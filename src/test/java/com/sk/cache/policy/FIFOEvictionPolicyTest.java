b974b70ebbcd33f5a451f60e6baac5bc17305c6d
Shambhavi <shambhavikhare.mahak@gmail.com>
2024-07-03 16:13:11 +0530

    Added UTs for all eviction policies

---
+++ src/test/java/com/sk/cache/policy/FIFOEvictionPolicyTest.java
@@ -0,0 +1,53 @@
+package com.sk.cache.policy;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FIFOEvictionPolicyTest {
+
+    private FifoEvictionPolicy<Integer> fifoEvictionPolicy;
+
+    @BeforeEach
+    void setUp() {
+        fifoEvictionPolicy = new FifoEvictionPolicy<>();
+    }
+
+    @Test
+    void testNoKeyToEvictInitially() {
+        assertNull(fifoEvictionPolicy.evictKey());
+    }
+
+    @Test
+    void testKeysAreEvictedInFirstInFirstOutOrder() {
+        fifoEvictionPolicy.keyAccessed(1);
+        fifoEvictionPolicy.keyAccessed(2);
+        fifoEvictionPolicy.keyAccessed(3);
+        fifoEvictionPolicy.keyAccessed(4);
+        
+        assertEquals(1, fifoEvictionPolicy.evictKey());
+        assertEquals(2, fifoEvictionPolicy.evictKey());
+        assertEquals(3, fifoEvictionPolicy.evictKey());
+        assertEquals(4, fifoEvictionPolicy.evictKey());
+    }
+
+    @Test
+    void testReaccessingKeyDoesNotChangeEvictionOrder() {
+        fifoEvictionPolicy.keyAccessed(1);
+        fifoEvictionPolicy.keyAccessed(2);
+        fifoEvictionPolicy.keyAccessed(3);
+        fifoEvictionPolicy.keyAccessed(2);
+        fifoEvictionPolicy.keyAccessed(4);
+        fifoEvictionPolicy.keyAccessed(1);
+        fifoEvictionPolicy.keyAccessed(5);
+        
+        assertEquals(1, fifoEvictionPolicy.evictKey());
+        assertEquals(2, fifoEvictionPolicy.evictKey());
+        assertEquals(3, fifoEvictionPolicy.evictKey());
+        assertEquals(4, fifoEvictionPolicy.evictKey());
+        assertEquals(5, fifoEvictionPolicy.evictKey());
+    }
+}
+
---
+++ src/test/java/com/sk/cache/policy/LIFOEvictionPolicyTest.java
@@ -0,0 +1,53 @@
+
+package com.sk.cache.policy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LIFOEvictionPolicyTest {
+
+    private LifoEvictionPolicy<Integer> lifoEvictionPolicy;
+
+    @BeforeEach
+    void setUp() {
+        lifoEvictionPolicy = new LifoEvictionPolicy<>();
+    }
+
+    @Test
+    void testNoKeyToEvictInitially() {
+        assertNull(lifoEvictionPolicy.evictKey());
+    }
+
+    @Test
+    void testKeysAreEvictedInLastInFirstOutOrder() {
+        lifoEvictionPolicy.keyAccessed(1);
+        lifoEvictionPolicy.keyAccessed(2);
+        lifoEvictionPolicy.keyAccessed(3);
+        lifoEvictionPolicy.keyAccessed(4);
+        
+        assertEquals(4, lifoEvictionPolicy.evictKey());
+        assertEquals(3, lifoEvictionPolicy.evictKey());
+        assertEquals(2, lifoEvictionPolicy.evictKey());
+        assertEquals(1, lifoEvictionPolicy.evictKey());
+    }
+
+    @Test
+    void testReaccessingKeyDoesNotChangeEvictionOrder() {
+        lifoEvictionPolicy.keyAccessed(1);
+        lifoEvictionPolicy.keyAccessed(2);
+        lifoEvictionPolicy.keyAccessed(3);
+        lifoEvictionPolicy.keyAccessed(2);
+        lifoEvictionPolicy.keyAccessed(4);
+        lifoEvictionPolicy.keyAccessed(1);
+        lifoEvictionPolicy.keyAccessed(5);
+        
+        assertEquals(5, lifoEvictionPolicy.evictKey());
+        assertEquals(1, lifoEvictionPolicy.evictKey());
+        assertEquals(4, lifoEvictionPolicy.evictKey());
+        assertEquals(3, lifoEvictionPolicy.evictKey());
+        assertEquals(2, lifoEvictionPolicy.evictKey());
+    }
+}
+
